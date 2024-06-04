e843ccdf61fbba95e643a2e40b591cda04f12420
thenuts98 <juno9804@naver.com>
2024-06-04 11:14:32 +0900

    "commit"

--- notice.py
+++ notice.py
@@ -2,28 +2,52 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = "https://lostark.game.onstove.com/News/Notice/List" # 로스트아크 공지페이지 주소
+url = "https://lostark.game.onstove.com/News/Notice" # 로스트아크 공지페이지 주소
 
 
-def get_notice(url = url):
-    """url에서 공지 목록 추출, list로 반환
+def get_notice_code(url = url + '/List'):
+    """url에서 공지 목록 추출, 코드 list로 반환\n
+    url에서 5자리 공지 코드를 추출합니다.\n
+    6자리 되기 전에 섭종할듯\n
+
     """
     with requests.get(url) as r:
-        
+
+        notice_list = [] 
         soup = BeautifulSoup(r.text, "html.parser")
-        notice_list = soup.find_all('ul')[11]
-        
-        notice_list = notice_list.find_all('a')
+
+        # 웹페이지별 하드 코딩
+        notice = soup.find_all('ul')[11] 
+        notice = notice.find_all('a')
         # print(notice_list)
 
-        for idx, i in enumerate(notice_list):
-            print(idx, i['href'][19:24])
-
+        for idx, i in enumerate(notice):
+            # print(idx, i['href'][19:24])
+            notice_list.append(i['href'][19:24])
 
 
     return notice_list
 
     
+def get_notice(code):
+    """ 주어진 코드 공지의 제목, 내용, 이미지 등 embed에 사용할 정보 구성
+    """
+    global url
+    notice_url = url + '/Views/' + str(code)
+    with requests.get(notice_url) as r:
+        soup = BeautifulSoup(r.text, "html.parser")
 
+        
+
+        print(soup.select('div.fr-view')[0].text.replace('.', '.\n'))
+
+        
+    
 if __name__ == '__main__':
-    get_notice()
+
+    notice_list = get_notice_code()
+
+    for i in notice_list:
+        get_notice(i)
+
+
